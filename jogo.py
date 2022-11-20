b2bd69a826477f7e208ca1dc367820010debae68
Luiz Miguel <luizmmb1@al.insper.edu.br>
2022-11-20 19:44:42 -0300

    adicao dos assets e player

--- jogo.py
+++ jogo.py
@@ -2,12 +2,23 @@ import pygame
 
 pygame.init()
 
-WIDTH = 500
-HEIGHT = 300
+#==== Tela principal
+WIDTH = 600
+HEIGHT = 500
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Medieval_MAN')
 
+#==== Assets inicias
+PLAYER_WIDTH = 17
+PLAYER_HEIGHT = 13
+
+assets = {}
+assets['player'] = pygame.image.load('assets/img/cavaleiro 2-1.png').convert_alpha()
+assets['player'] = pygame.transform.scale(assets['player'],(PLAYER_WIDTH,PLAYER_HEIGHT))
+
+#==== Loop principal
 game = True
+
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -15,4 +26,5 @@ while game:
     
     window.fill((0,0,0))
     pygame.display.update()
+
 pygame.quit()
