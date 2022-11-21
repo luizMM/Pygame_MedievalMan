adb8ead180b39d62122ec5ad255b6a61e1ee576c
Luiz Guilherme <lguime2004@gmail.com>
2022-11-21 17:31:35 -0300

    Arruma limite da tela e coloca as descrições

--- jogo.py
+++ jogo.py
@@ -1,18 +1,23 @@
+#importa pacotes
+#inicialização
 import pygame
 
 pygame.init()
 
+#FPS e tamanhos
 FPS = 30
 TELA_WIDTH = 600
-TELA_HEIGHT = 800
+TELA_HEIGHT = 650
 L_MAPA = 224*2
 C_MAPA = 288*2
-janela = pygame.display.set_mode((TELA_WIDTH,TELA_HEIGHT))
-pygame.display.set_caption('Medieval_man')
-
 PLAYER_WIDTH = 50
 PLAYER_HEIGHT = 45
 
+#Telas principal do jogo
+janela = pygame.display.set_mode((TELA_WIDTH,TELA_HEIGHT))
+pygame.display.set_caption('Medieval_man')
+
+#assets
 def load_assets():
     assets = {}
     assets['background'] = pygame.image.load('assets/img/Mapa v1-1.png.png').convert_alpha()
@@ -21,33 +26,37 @@ def load_assets():
     assets['player'] = pygame.transform.scale(assets['player'],(PLAYER_WIDTH,PLAYER_HEIGHT))
     return assets
 
+#classe jogador
 class Player(pygame.sprite.Sprite):
     def __init__(self,groups,assets):
         pygame.sprite.Sprite.__init__(self)
-
+        #Construção personagem
         self.image = assets['player']
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.centerx = TELA_WIDTH/2
-        self.rect.bottom = TELA_HEIGHT - 10
+        self.rect.bottom = TELA_HEIGHT
         self.speedx = 0
         self.speedy = 0
         self.groups = groups
         self.assets = assets
     def update(self):
+        #atualiza posição da nave
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-
-        if self.rect.top > TELA_HEIGHT:
-            self.rect.top = TELA_HEIGHT
-        if self.rect.bottom < 0:
-            self.rect.bottom = 0
+        #mantém dento da tela
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > TELA_HEIGHT:
+            self.rect.bottom = TELA_HEIGHT
         if self.rect.right > TELA_WIDTH:
             self.rect.right = TELA_WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
 
+#tela do jogo
 def game_screen(janela):
+    #clock(fps)
     clock = pygame.time.Clock()
 
     assets = load_assets()
@@ -65,15 +74,18 @@ def game_screen(janela):
     JOGANDO = 1
     state = JOGANDO
 
+    #loop principal
     while state != ACABOU:
         clock.tick(FPS)
-
+        #Trata eventos
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 state = ACABOU
             if state == JOGANDO:
+                #verifica se apertou a tecla
                 if event.type == pygame.KEYDOWN:
                     key_downs[event.key] = True
+                    #verifica quais teclas foram apertadas
                     if event.key == pygame.K_UP:
                         player.speedy -= 8
                     if event.key == pygame.K_DOWN:
@@ -82,6 +94,7 @@ def game_screen(janela):
                         player.speedx -= 8
                     if event.key == pygame.K_RIGHT:
                         player.speedx += 8
+                #verifica se soltou a tecla
                 if event.type == pygame.KEYUP:
                     if event.key in key_downs and key_downs[event.key]:
                         if event.key == pygame.K_UP:
@@ -92,13 +105,15 @@ def game_screen(janela):
                             player.speedx += 8
                         if event.key == pygame.K_RIGHT:
                             player.speedx -= 8
+        #Atualiza posição do personagem
         all_sprites.update()
 
+        #Gera saídas
         janela.fill((0,0,0))
-        janela.blit(assets['background'],(80,90))
+        janela.blit(assets['background'],(80,0))
         all_sprites.draw(janela)
 
         pygame.display.update()
 game_screen(janela)
-
+#finalização
 pygame.quit()
